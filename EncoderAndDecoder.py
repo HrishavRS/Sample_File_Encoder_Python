826569e9c6253f504801ea3cb89469b97af73f83
Hrishav <megahrishav@gmail.com>
2022-03-18 23:32:19 +0545

    Adding comments from branch1

--- EncoderAndDecoder.py
+++ EncoderAndDecoder.py
@@ -1,7 +1,10 @@
 import os
 
-this_array = [["a",71],["b",82],["c",93],["d",64],["e",55],["f",73],["g",67],["h",80],["i",90],["j",10],["k",11],["l",12],["m",13],["n",14],["o",15],["p",16],["q",17],["r",18],["s",19],["t",20],["u",21],["v",22],["w",23],["x",24],["y",25],["z",26],[".",79],["A",27],["B",28],["C",29],["D",74],["E",30],["F",31],["G",32],["H",33],["I",34],["J",35],["K",36],["L",37],["M",38],["N",39],["O",40],["P",41],["Q",42],["R",43],["S",44],["T",45],["U",46],["V",47],["W",48],["X",49],["Y",50],["Z",51],['0',88],['1',70],['2',60],['3',94],['4',95],['5',96],['6',97],['7',98],['8',99],['9',89],['_',83],[' ',75]]
+#This Encoding is done based on values from array below.
+#if the letter is a then it's 71, 64 for d, 90 for i etc etc...
+this_array = [["a",71],["b",82],["c",93],["d",64],["e",55],["f",73],["g",67],["h",80],["i",90],["j",10],["k",11],["l",12],["m",13],["n",14],["o",15],["p",16],["q",17],["r",18],["s",19],["t",20],["u",21],["v",22],["w",23],["x",24],["y",25],["z",26],[".",79],["A",27],["B",28],["C",29],["D",74],["E",30],["F",31],["G",32],["H",33],["I",34],["J",35],["K",36],["L",37],["M",38],["N",39],["O",40],["P",41],["Q",42],["R",43],["S",44],["T",45],["U",46],["V",47],["W",48],["X",49],["Y",50],["Z",51],['0',88],['1',70],['2',60],['3',94],['4',95],['5',96],['6',97],['7',98],['8',99],['9',89],['_',83],[' ',75],['\'',72]]
 
+#The below function reverses the encoding procedure
 def revertor(encod):
 	encod = str(encod)
 	numbers_in_2_groups=[]
@@ -22,6 +25,8 @@ def revertor(encod):
 	final_reverted_item = ''.join(map(str,final_reverted_item))			
 	return(final_reverted_item)
 
+#the below function is the black mamba, THE ENCODER
+#writing in comments feels like being a ghost-guide, Even the words a grey.
 def convertor(filename):
 	final_array = []
 	for item in list(filename):
@@ -31,6 +36,8 @@ def convertor(filename):
 				concat_form = ''.join(map(str,final_array))
 	return concat_form
 
+
+#You can use this to encode your files just like i tried below
 # files_in_current_Dir = os.listdir()
 # print(files_in_current_Dir)
 # encoded_files = []
@@ -39,6 +46,8 @@ def convertor(filename):
 # 	os.rename(i,x)
 
 # reverted_files=[]
-y = revertor(749018559344712255791625)
+
+test = 27121890678020751415237525152175111415237580152375902072197564151455751915756715156475122193117555149315649014677975271219157534756415147220751114152375238025752515217571185575211990146775208090197525152175192021169064759064901520
+
+y = revertor(x)
 print(y)
-# os.rename(7490185593447122553855791625,y)
