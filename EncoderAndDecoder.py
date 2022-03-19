48fbef76ec4a010e68f97965ff8560fd3c72ef52
Hrishav <megahrishav@gmail.com>
2022-03-19 21:50:06 +0545

    Adding Directory Reading python file

--- EncoderAndDecoder.py
+++ EncoderAndDecoder.py
@@ -27,8 +27,6 @@ def revertor(encod):
 	final_reverted_item = ''.join(map(str,final_reverted_item))			
 	return(final_reverted_item)
 
-#the below function is the black mamba, THE ENCODER
-#writing in comments feels like being a ghost-guide, Even the words a grey.
 def convertor(filename):
 	final_array = []
 	for item in list(filename):
@@ -38,18 +36,3 @@ def convertor(filename):
 				concat_form = ''.join(map(str,final_array))
 	return concat_form
 
-
-#You can use this to encode your files just like i tried below
-# files_in_current_Dir = os.listdir()
-# print(files_in_current_Dir)
-# encoded_files = []
-# for i in files_in_current_Dir:
-# 	x = convertor(i)
-# 	os.rename(i,x)
-
-# reverted_files=[]
-
-test = 27121890678020751415237525152175111415237580152375902072197564151455751915756715156475122193117555149315649014677975271219157534756415147220751114152375238025752515217571185575211990146775208090197525152175192021169064759064901520
-
-y = revertor(x)
-print(y)
